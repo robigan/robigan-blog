1cc136ae80fdc79af2f394a2adbe0ced8574ea13
robigan <35210888+robigan@users.noreply.github.com>
2022-12-06 15:28:52 +0100

    Make changes to Tina config

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,46 +1,121 @@
 
-  import { defineConfig } from "tinacms";
-  
-  // Your hosting provider likely exposes this as an environment variable
-  const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-  
-  export default defineConfig({
-    branch,
-    clientId: null,   // Get this from tina.io
-    token: null,      // Get this from tina.io
-    build: {
-      outputFolder: "admin",
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null,   // Get this from tina.io
+  token: null,      // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
       publicFolder: "static",
     },
-    media: {
-      tina: {
-        mediaRoot: "uploads",
-        publicFolder: "static/uploads",
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Meta Description",
+            required: true,
+            ui: {
+              component: "textarea"
+            }
+          },
+          {
+            type: "string",
+            name: "summary",
+            label: "Summary",
+            required: false
+          },
+          {
+            type: "string",
+            name: "robots",
+            label: "Robots Configuration",
+            description: "Google Search Engine Robots Meta Tag value. See https://developers.google.com/search/docs/crawling-indexing/robots-meta-tag#directives"
+          },
+          {
+            type: "string",
+            name: "menu",
+            label: "Menu",
+            options: [
+              {
+                label: "Main",
+                value: "main"
+              },
+              {
+                label: "Footer",
+                value: "footer"
+              }
+            ],
+            required: false,
+            description: "When a value is provided, a link to this article will appear in the named menus."
+          },
+          {
+            type: "string",
+            name: "series",
+            label: "Series",
+            list: true,
+            required: false,
+            description: "Array of series the article belongs to, we recommend using only one series per article."
+          },
+          {
+            type: "number",
+            name: "series_order",
+            label: "Series Order",
+            required: false,
+            description: "Position of the article within the series."
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date Published",
+          },
+          {
+            type: "datetime",
+            name: "lastmod",
+            label: "Last Modified Date"
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true,
+            required: false
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft",
+            description: "Whether or not this article is considered a rough draft by Hugo."
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
       },
-    },
-    schema: {
-      collections: [
-        {
-          name: "post",
-          label: "Posts",
-          path: "content/posts",
-          fields: [
-            {
-              type: "string",
-              name: "title",
-              label: "Title",
-              isTitle: true,
-              required: true,
-            },
-            {
-              type: "rich-text",
-              name: "body",
-              label: "Body",
-              isBody: true,
-            },
-          ],
-        },
-      ],
-    },
-  });
-  
+    ],
+  },
+});
